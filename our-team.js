1161e7c2064a7a90c0e8d5f5b8f4ed7d632d66d6
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-06-27 19:47:51 -0400

    Mobile Gradient Background Added

--- our-team.js
+++ our-team.js
@@ -1,16 +1,10 @@
-const siteNav = document.querySelectorAll("nav a");
-siteNav[0].textContent = "Home";
-siteNav[1].textContent = "The App";
-siteNav[2].textContent = "The Story";
-siteNav[3].textContent = "The Team";
-siteNav[4].textContent = "Log In/Sign Up";
 
 const floatingNav = document.querySelector("header");
 floatingNav.addEventListener('reset', () => {
     floatingNav.preventDefault();
 });
 
-const orangeNav = document.querySelector("header nav a");
+const orangeNav = document.querySelector("h1");
 orangeNav.addEventListener('mouseover', () => {
   orangeNav.style.color = 'orange';
 });
